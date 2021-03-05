92bc442c394e137092a55b855016a3b8a8cfea14
OstPerino <65489773+OstPerino@users.noreply.github.com>
2021-03-06 02:57:31 +0700

    New key
    Key now could be an index for all the string which user has typped

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,13 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QMessageBox>
+#include <QString>
+
+const QString upperCase = " ABCDEGHIJKLMNOPQRSTUVWXYZ";
+const QString lowerCase = " abcdegfijklmnopqrstuwxyz";
+
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -13,3 +20,21 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_pushButton_clicked()
+{
+    QString key = ui->keyEdit->text();
+    QString userInput = ui->stringEdit->text();
+
+    int count = 0;
+
+    while (key.length() < userInput.length())
+    {
+        key.append(key[count]);
+        count++;
+        if (count == key.length())
+            count = 0;
+    }
+
+
+}
