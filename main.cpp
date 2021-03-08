c24460d847946e4b2e3b1ebec0a6c81cff9d7038
OstPerino <65489773+OstPerino@users.noreply.github.com>
2021-03-08 14:52:52 +0700

    Set stylesheet for app
    stylesheet is done, have taken a good color combination for user

--- main.cpp
+++ main.cpp
@@ -5,6 +5,11 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setStyleSheet("* {font-family: century gothic; font-size: 20px}"
+                    "QMainWindow {background-color: #222831}"
+                    "QLabel {color: #ffd369}"
+                    "QLineEdit {background-color: #393e46; color: #ffd369;}"
+                    "QPushButton {background-color: #393e46; color: #ffd369}");
     MainWindow w;
     w.show();
     return a.exec();
